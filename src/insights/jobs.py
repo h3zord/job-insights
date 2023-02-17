7ea4a89c5c73528e10f6aab381585bff7de4d5fe
Lucas <lucaschmain@gmail.com>
2023-02-17 12:06:34 -0300

    Completing requirement 06

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -21,18 +21,11 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    job_type_list = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            new_dict = {}
+            new_dict["id"] = job["id"]
+            new_dict["job_type"] = job_type
+            job_type_list.append(new_dict)
+    return job_type_list
