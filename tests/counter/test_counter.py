51c01f8e1972c51d1b0f509b4224dd976b2b4dfe
Lucas <lucaschmain@gmail.com>
2023-02-18 00:19:54 -0300

    Completing requirement 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    count_ocurrences('data/jobs.csv', 'Python')
+    assert type('data/jobs.csv') == str
+    assert count_ocurrences('data/jobs.csv', 'Python') == 1639
