2a9b353b32eae87cd361e9b2b28d9608399fc806
Lucas <lucaschmain@gmail.com>
2023-02-18 10:34:22 -0300

    Completing requirement 11 and 12

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,10 @@
-# from src.pre_built.sorting import sort_by
+from src.pre_built.sorting import sort_by
+from src.insights.jobs import read
+
+
+dict_list = read("data/jobs.csv")
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(dict_list, "min_salary")
+    assert dict_list[0]["id"] == "2191"
